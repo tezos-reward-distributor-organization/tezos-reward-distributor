f5903c1c0bbc31e6e18d705a6f393e9d7d669b30
Huseyin ABANOZ <huseyinabanox@gmail.com>
2018-10-31 00:45:30 +0300

    counter ters

--- BatchPaymentTest.py
+++ BatchPaymentTest.py
@@ -1,19 +1,26 @@
 import os
 import subprocess
 
-from ClientConfiguration import COMM_HASH
-
-my_env = os.environ.copy()
-my_env["TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER"] = "Y"
-
+from ClientConfiguration import COMM_HASH, COMM_COUNTER
 from NetworkConfiguration import network_config_map
-print(my_env)
-network_config = network_config_map["ZERONET"]
-process = subprocess.Popen(COMM_HASH.replace("%network%", network_config['NAME'].lower()), shell=True, stdout=subprocess.PIPE,env=my_env,bufsize=1,universal_newlines=True)
-line=None
-for l in process.stdout:
-    line=l
-    
-print("hash is {}".format(line))
 
-process.wait()
+
+def run_and_last_line(cmd):
+    my_env = os.environ.copy()
+    my_env["TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER"] = "Y"
+
+    network_config = network_config_map["ZERONET"]
+    process = subprocess.Popen(cmd.replace("%network%", network_config['NAME'].lower()), shell=True,
+                               stdout=subprocess.PIPE, env=my_env, bufsize=1, universal_newlines=True)
+    line = None
+    for l in process.stdout:
+        line = l.strip("\"")
+    process.wait()
+    return line
+
+
+hash = run_and_last_line(COMM_HASH)
+counter = int(run_and_last_line(COMM_COUNTER))
+
+print("hash is {}".format(hash))
+print("counter is {}".format(counter))
--- ClientConfiguration.py
+++ ClientConfiguration.py
@@ -4,15 +4,18 @@
 # CLIENT_PATH = "~/tezos/tezos-client"
 
 # if docker images are used, %network% will be replaced by mainnet, zeronet, alphanet values
+from BussinessConfiguration import BAKING_ADDRESS
+
 CLIENT_PATH = "~/%network%.sh client"
 
 NODE_ADDR = "127.0.0.1"
 NODE_PORT = "8732"
 NODE_URL = NODE_ADDR + ":" + NODE_PORT
 
-SHELL_COMM_DISABLE_DISCLAIMER = "export TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y"
-
 # transfer command
 COMM_TRANSFER = CLIENT_PATH + " transfer {0:f} from {1} to {2} --fee 0"
 
 COMM_HASH = CLIENT_PATH + " rpc get http://{}/chains/main/blocks/head/hash".format(NODE_URL)
+
+COMM_COUNTER = CLIENT_PATH + "rpc get http://{}/chains/main/blocks/head/context/contracts/{}/counter".format(NODE_URL,
+                                                                                                             BAKING_ADDRESS)
