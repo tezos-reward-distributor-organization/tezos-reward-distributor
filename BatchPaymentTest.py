3d14a65352f9c8643159c16396e88243233526b0
Huseyin ABANOZ <huseyinabanox@gmail.com>
2018-10-31 00:03:56 +0300

    process output

--- BatchPaymentTest.py
+++ BatchPaymentTest.py
@@ -7,4 +7,9 @@ from NetworkConfiguration import network_config_map
 
 network_config = network_config_map["ZERONET"]
 process = subprocess.Popen(COMM_HASH.replace("%network%", network_config['NAME'].lower()), shell=True, stdout=subprocess.PIPE)
+
+while process.poll() is None:
+    output = process.stdout.readline()
+    print(output),
+
 process.wait()
