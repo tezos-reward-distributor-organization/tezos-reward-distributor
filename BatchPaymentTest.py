67eb3b8c66dc50e78ecb0fd44435458d542d5b9b
Huseyin ABANOZ <huseyinabanox@gmail.com>
2018-10-31 00:36:57 +0300

    hash print

--- BatchPaymentTest.py
+++ BatchPaymentTest.py
@@ -7,11 +7,13 @@ my_env = os.environ.copy()
 my_env["TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER"] = "Y"
 
 from NetworkConfiguration import network_config_map
-
+print(my_env)
 network_config = network_config_map["ZERONET"]
 process = subprocess.Popen(COMM_HASH.replace("%network%", network_config['NAME'].lower()), shell=True, stdout=subprocess.PIPE,env=my_env,bufsize=1,universal_newlines=True)
-
-for line in process.stdout:
-    print(line,end='')
+line=None
+for l in process.stdout:
+    line=l
+    
+print("hash is {}".format(line))
 
 process.wait()
