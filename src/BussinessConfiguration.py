797be81f7a8bc1a8a7cb7322cc3915e4a12232a4
Huseyin ABANOZ <huseyinabanox@gmail.com>
2018-11-02 19:06:09 +0300

    test

--- src/BussinessConfiguration.py
+++ src/BussinessConfiguration.py
@@ -6,8 +6,8 @@ BAKING_ADDRESS = "tz1YZReTLamLhyPLGSALa4TbMhjjgnSi2cqP"
 # standard fee that is valid for everybody
 STANDARD_FEE=0.045
 # founders that shares the profits. map of founder payment address and share of the profit. Shares should sum up to 1.
-founders_map={"KT1MMhmTkUoHez4u58XMZL7NkpU9FWY4QLn2":1.0}
-# deposit owners map of address and deposit ratio which must sum upto 1
+founders_map={"tz1MWTkFRXA2dwez4RHJWnDWziLpaN6iDTZ9":1.0}
+# deposit owners map of address and deposit ratio which must sum up to 1
 owners_map={"KT1MMhmTkUoHez4u58XMZL7NkpU9FWY4QLn2":1.0}
 # no fee customers, e.g. founders, supporters
 supporters_set={"KT1T4RgrQ986qp2ndZ2YTnxKVSSUrD2BoKFH","KT1MMhmTkUoHez4u58XMZL7NkpU9FWY4QLn2"}
