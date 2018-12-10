3079d7c056a34fe700200d8febb28462c7eac6d2
Huseyin ABANOZ <huseyinabanox@gmail.com>
2018-12-10 08:25:24 +0300

    find right error desc

--- src/pay/batch_payer.py
+++ src/pay/batch_payer.py
@@ -149,7 +149,13 @@ class BatchPayer():
         if verbose: logger.debug("runops_command_str is |{}|".format(runops_command_str))
         runops_command_response = send_request(runops_command_str, verbose)
         if not check_response(runops_command_response):
-            logger.error("Error in run_operation response '{}'".format(runops_command_response["contents"]["metadata"]["operation_result"]))
+            error_desc=runops_command_response
+            for content in runops_command_response["contents"]:
+                op_result = content["metadata"]["operation_result"]
+                if op_result["status"] == 'failed':
+                    error_desc = op_result["errors"]
+                    break
+            logger.error("Error in run_operation response '{}'".format(error_desc))
             return False, ""
 
         # forge the operations
