bf63262438561b54b01ef8d6eb577f227f47d638
Huseyin ABANOZ <huseyinabanox@gmail.com>
2019-02-18 22:51:36 +0300

    python executable absoule path

--- service_add.py
+++ service_add.py
@@ -6,6 +6,7 @@ def main():
     path_template = "tezos-reward.service_template"
     dir_path = os.path.dirname(os.path.realpath(__file__))
     path_service = os.path.join(dir_path, "tezos-reward.service")
+    python_executable=sys.executable
 
     # if len(sys.argv)==1:
     #    print("ERROR: Arguments not given. See list of arguments below:")
@@ -13,6 +14,7 @@ def main():
 
     with open(path_template, 'r') as template_file:
         content = template_file.read()
+        content = content.replace("<PYTHON_PATH>", python_executable)
         content = content.replace("<ABS_PATH_TO_BASE>", dir_path)
         content = content.replace("<OPTIONS>", ' '.join(sys.argv[1:]))
 
