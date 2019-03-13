fda368d44718aa6ac295d1eb1dacf2b895321524
Huseyin ABANOZ <huseyinabanox@gmail.com>
2019-03-13 08:52:26 +0300

    PaymentRecord.FromPaymentCSVDictRow fix

--- src/model/payment_log.py
+++ src/model/payment_log.py
@@ -54,6 +54,6 @@ class PaymentRecord():
         return items
 
     def __str__(self):
-        return "cycle = {}, address={}, ratio={}, fee_rate={}, reward={}, fee={}, type={}, payment={}, paid={}, hash={}" \
+        return "cycle = {}, address={}, type={}, payment={}, paid={}, hash={}" \
             .format(self.cycle, self.address, self.ratio, self.fee_rate, self.reward, self.fee, self.type, self.payment,
                     self.paid, self.hash)
