feb59c82301438735f16183062a58a7072e28acb
Huseyin ABANOZ <huseyinabanox@gmail.com>
2019-04-20 10:50:29 +0300

    stats publisher url update

--- src/stats/stats_pusblisher.py
+++ src/stats/stats_pusblisher.py
@@ -1,10 +1,12 @@
 import json
+from time import sleep
+
 import requests
 import threading
 import time
 import datetime
 
-url = "http://172.30.10.8:8080/trd/stats/"
+url = "http://34.74.27.213:8080/trd/stats/add"
 headers = {'content-type': 'application/json'}
 
 
@@ -12,7 +14,7 @@ def stat_publish(stats_map):
     stats_map['time_zone'] = time.timezone / -(60 * 60)  # e.g. +3
     stats_map['time'] = str(datetime.datetime.now())  # e.g. '2017-03-12 22:29:03.066794'
 
-    t = threading.Thread(target=stat_publish_job, name="stat_publish_job", args=(stats_map,))
+    t = threading.Thread(target=stat_publish_job, name="stat_publish_job", args=({'stats':stats_map},))
     t.daemon = True
     t.start()
 
@@ -26,4 +28,16 @@ def stat_publish_job(stats_map):
 
 
 if __name__ == '__main__':
-    stat_publish({"a": "b", "c": "d"})
+    stats_dict = {}
+    stats_dict['total_amount'] = 123
+    stats_dict['nb_payments'] = 12
+    stats_dict['nb_failed'] = 0
+    stats_dict['nb_founder'] = 1
+    stats_dict['nb_owner'] = 1
+    stats_dict['nb_delegator'] = 10
+    stats_dict['cycle'] = 83
+    stats_dict['delegator_pays_fee'] = 1
+
+    stat_publish(stats_dict)
+
+    sleep(1)
