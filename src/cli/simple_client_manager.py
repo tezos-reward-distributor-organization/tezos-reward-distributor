a94882887145557eca3935163a746c2250d19ffc
Huseyin ABANOZ <huseyinabanox@gmail.com>
2019-06-08 10:16:16 +0300

    sign

--- src/cli/simple_client_manager.py
+++ src/cli/simple_client_manager.py
@@ -15,11 +15,13 @@ class SimpleClientManager:
         return self.cmd_manager.execute(whole_cmd, verbose_override, timeout=timeout)
 
     def sign(self, bytes, key_name):
-        response = self.send_request(" sign bytes 0x03{} for {}".format(bytes, key_name))
+        result, response = self.send_request(" sign bytes 0x03{} for {}".format(bytes, key_name))
+
+        if not result:
+            raise ClientException("Error at signing: '{}'".format(response))
 
         for line in response.splitlines():
             if "Signature" in line:
                 return line.strip("Signature:").strip()
 
-        raise ClientException(
-            "Signature not found in response '{}'. Signed with {}".format(response.replace('\n'), 'key_name'))
+        raise ClientException("Signature not found in response '{}'. Signed with key '{}'".format(response, key_name))
