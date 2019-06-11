bf42398ca7c215efab0ce509aa1b6bda2873eb11
Huseyin ABANOZ <huseyinabanox@gmail.com>
2019-06-11 16:31:19 +0300

    Python version check added. Python 3.6 or later is needed. version 5.6

--- src/pay_for.py
+++ src/pay_for.py
@@ -180,9 +180,8 @@ class ReleaseOverrideAction(argparse.Action):
 
 if __name__ == '__main__':
 
-    if sys.version_info[0] < 3:
-        raise Exception("Must be using Python 3")
-
+    if not sys.version_info.major >= 3 and sys.version_info.minor>=6:
+        raise Exception("Must be using Python 3.6 or later but it is {}.{}".format(sys.version_info.major,sys.version_info.minor ))
 
     parser = argparse.ArgumentParser()
     add_argument_network(parser)
--- src/retry_failed.py
+++ src/retry_failed.py
@@ -178,8 +178,8 @@ def get_baking_configuration_file(config_dir):
 
 if __name__ == '__main__':
 
-    if sys.version_info[0] < 3:
-        raise Exception("Must be using Python 3")
+    if not sys.version_info.major >= 3 and sys.version_info.minor>=6:
+        raise Exception("Must be using Python 3.6 or later but it is {}.{}".format(sys.version_info.major,sys.version_info.minor ))
 
     parser = argparse.ArgumentParser()
     add_argument_network(parser)
