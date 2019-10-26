47a041a1f51d9e4f7a4f3aacba4e21076848a3b1
amzid <ahmed.mzid@gmail.com>
2019-10-26 12:59:35 +0200

    adjust unit tests with the provider class changes

--- src/calc/test_calculatePhase0.py
+++ src/calc/test_calculatePhase0.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from calc.calculate_phase0 import CalculatePhase0
 from model import reward_log
-from rpc.prpc_reward_api import PRpcRewardApiImpl
+from api.provider_factory import ProviderFactory
 
 BAKING_ADDRESS = "tz1Z1tMai15JWUWeN2PKL9faXXVPMuWamzJj"
 
@@ -14,7 +14,7 @@ class TestCalculatePhase0(TestCase):
         nw = {'NAME': 'MAINNET', 'NB_FREEZE_CYCLE': 5, 'BLOCK_TIME_IN_SEC': 60, 'BLOCKS_PER_CYCLE': 4096,
                 'BLOCKS_PER_ROLL_SNAPSHOT': 256}
 
-        api = PRpcRewardApiImpl(nw,  BAKING_ADDRESS, "mainnet.tezrpc.me")
+        api = ProviderFactory(provider='prpc').newRewardApi(nw,  BAKING_ADDRESS, '')
         model = api.get_rewards_for_cycle_map(153)
 
         phase0 = CalculatePhase0(model)
