d4593c40858aa37d216ccecadfe2eeb5cbab12bb
amzid <ahmed.mzid@gmail.com>
2019-11-02 12:20:41 +0100

    Fix bug

--- src/tzstats/tzstats_reward_provider_helper.py
+++ src/tzstats/tzstats_reward_provider_helper.py
@@ -7,6 +7,7 @@ from tzstats.tzstats_api_constants import *
 logger = main_logger
 
 rewards_split_call = '/tables/income?address={}&cycle={}'
+delegators_call = '/tables/snapshot?cycle={}&is_selected=1&delegate={}&limit=50000'
 
 PREFIX_API = {'MAINNET': {'API_URL': 'http://api.tzstats.com'},
               'ZERONET': {'API_URL': 'http://api.zeronet.tzstats.com'},
@@ -18,6 +19,8 @@ class TzStatsRewardProviderHelper:
 
     def __init__(self, nw, baking_address):
         super(TzStatsRewardProviderHelper, self).__init__()
+
+        self.preserved_cycles = nw['NB_FREEZE_CYCLE']
 
         self.api = PREFIX_API[nw['NAME']]
         if self.api is None:
@@ -45,8 +48,26 @@ class TzStatsRewardProviderHelper:
 
         resp = resp.json()[0]
 
-        root["total_reward_amount"] = resp[idx_income_total_income] * 1e6 # needs to be more precise -> feature request
-        root["delegate_staking_balance"] = resp[idx_income_balance] + resp[idx_income_delegated]
-        # root["delegators_balance"] -> feature request
+        root["total_reward_amount"] = int( 1e6 * ( float(resp[idx_income_baking_income]) +  float(resp[idx_income_endorsing_income]) + float(resp[idx_income_seed_income]) +  float(resp[idx_income_fees_income])  -  float(resp[idx_income_lost_accusation_fees]) -  float(resp[idx_income_lost_accusation_rewards]) -  float(resp[idx_income_lost_revelation_fees]) -  float(resp[idx_income_lost_revelation_rewards])))
+        root["delegate_staking_balance"] = int( 1e6 * (float(resp[idx_income_balance]) + float(resp[idx_income_delegated])))
+
+        uri = self.api['API_URL'] + delegators_call.format(cycle - self.preserved_cycles - 2, self.baking_address)
+
+        if verbose:
+            logger.debug("Requesting {}".format(uri))
+
+        resp = requests.get(uri, timeout=5)
+
+        if verbose:
+            logger.debug("Response from tzstats is {}".format(resp))
+
+        if resp.status_code != 200:
+            # This means something went wrong.
+            raise TzStatsException('GET {} {}'.format(uri, resp.status_code))
+
+        resp = resp.json()
+
+        for delegator in resp:
+            root["delegators_balance"][delegator[idx_delegator_address]] = int(1e6 * float(delegator[idx_delegator_balance]))
 
         return root
