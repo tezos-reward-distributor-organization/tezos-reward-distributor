3e8eacbc0236563fa37b126e0adc400486b69726
utdrmac <github@matthewboehm.com>
2020-03-27 17:08:10 +0000

    add missing node addr parameter

--- src/retry_failed.py
+++ src/retry_failed.py
@@ -79,7 +79,7 @@ def main(args):
     logger.debug("Tezos client path is {}".format(client_path))
 
     # 4. get network config
-    config_client_manager = SimpleClientManager(client_path)
+    config_client_manager = SimpleClientManager(client_path, args.node_addr)
     network_config_map = init_network_config(args.network, config_client_manager, args.node_addr)
     network_config = network_config_map[args.network]
 
