439af707573b0d03b42af8c360fa562dc03bb50c
Matthew <4413670+utdrmac@users.noreply.github.com>
2020-11-22 10:04:13 -0600

    Swap pycodestyle for flake8 to include code linting (#320)
    * fix issue checklist template
    * Contributor: utdrmac, Effort=2h
    * Reviewer: amzid, Effort=0.5h

--- src/rpc/rpc_reward_api.py
+++ src/rpc/rpc_reward_api.py
@@ -113,7 +113,7 @@ class RpcRewardApiImpl(RewardApi):
 
         try:
             resp = requests.get(request, timeout=time_out)
-        except requests.exceptions.Timeout as e:
+        except requests.exceptions.Timeout:
             # Catches both ConnectTimeout and ReadTimeout
             message = "[do_rpc_request] Requesting URL '{:s}' timed out after {:d}s".format(request, time_out)
             logger.error(message)
