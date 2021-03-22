bfa6e6e46322e81c373d22edc47d40954f527c7d
Huseyin ABANOZ <huseyinabanox@gmail.com>
2021-03-22 21:05:55 +0300

    setup py

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(
-    name='tezos-reward-distributor-branch',
-    version='6.0',
-    packages=['api', 'cli', 'pay', 'rpc', 'calc', 'tzkt', 'util', 'model', 'stats', 'config', 'Dexter', 'plugins',
-              'tzstats', 'exception'],
-    package_dir={'': 'src'},
-    url='',
-    license='',
-    author='trd-team',
-    author_email='',
-    description=''
-)
+setup(name="PACKAGENAME", packages=find_packages())
