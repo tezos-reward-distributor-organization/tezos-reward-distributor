81c23464f6bbc510040dd24eafdc224dc5fd913f
Viktor Kreschenski <viktor.kreschenski@gmail.com>
2021-05-15 22:43:27 +0200

    Install graphviz for tests

--- tests/unit/test_state_graphs.py
+++ tests/unit/test_state_graphs.py
@@ -5,17 +5,17 @@ def test_draw_process_life_cycle():
     try:
         from util.process_life_cycle import ProcessLifeCycle
         process_life_cycle = ProcessLifeCycle(None)
-        process_life_cycle.get_fsm_builder().draw("/tmp/launch_state_diagram.png")
-        assert os.path.isfile("/tmp/launch_state_diagram.png") is True
+        process_life_cycle.get_fsm_builder().draw("launch_state_diagram.png")
+        assert os.path.isfile("launch_state_diagram.png") is True
     finally:
-        os.remove("/tmp/launch_state_diagram.png")
+        os.remove("launch_state_diagram.png")
 
 
 def test_draw_config_life_cycle():
     try:
         from util.config_life_cycle import ConfigLifeCycle
         config_life_cycle = ConfigLifeCycle(None, None, None, None)
-        config_life_cycle.get_fsm_builder().draw("/tmp/config_cycle_state_diagram.png")
-        assert os.path.isfile("/tmp/config_cycle_state_diagram.png") is True
+        config_life_cycle.get_fsm_builder().draw("config_cycle_state_diagram.png")
+        assert os.path.isfile("config_cycle_state_diagram.png") is True
     finally:
-        os.remove("/tmp/config_cycle_state_diagram.png")
+        os.remove("config_cycle_state_diagram.png")
