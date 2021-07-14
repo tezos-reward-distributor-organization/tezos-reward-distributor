69d50f48a1e094b894c802b8060dddb9f79271ce
Viktor Kreschenski <viktor.kreschenski@gmail.com>
2021-07-14 20:27:17 +0200

    Bump 3.8 -> 3.9

--- docs/fsm/graphviz/draw_state_diagrams.py
+++ docs/fsm/graphviz/draw_state_diagrams.py
@@ -1,8 +1,11 @@
-from util.config_life_cycle import ConfigLifeCycle
-from util.process_life_cycle import ProcessLifeCycle
+def draw_state_diagram():
+    from util.config_life_cycle import ConfigLifeCycle
+    from util.process_life_cycle import ProcessLifeCycle
+    process_life_cycle = ProcessLifeCycle(None)
+    config_life_cycle = ConfigLifeCycle(None, None, None, None)
 
-process_life_cycle = ProcessLifeCycle(None)
-config_life_cycle = ConfigLifeCycle(None, None, None, None)
+    process_life_cycle.get_fsm_builder().draw("docs/fsm/graphviz/launch_state_diagram.png", title="Process Life Cycle")
+    config_life_cycle.get_fsm_builder().draw("docs/fsm/graphviz/config_cycle_state_diagram.png", title="Configuration Life Cycle")
 
-process_life_cycle.get_fsm_builder().draw("docs/fsm/graphviz/launch_state_diagram.png", title="Process Life Cycle")
-config_life_cycle.get_fsm_builder().draw("docs/fsm/graphviz/config_cycle_state_diagram.png", title="Configuration Life Cycle")
+if __name__ == "__main__":
+    draw_state_diagram()
