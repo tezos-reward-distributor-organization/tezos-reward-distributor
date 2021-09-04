2bfc04f7eb83762244a31a44fbdfa3cb58e1f390
utdrmac <github@matthewboehm.com>
2021-09-04 17:38:14 +0000

    more flake

--- tests/regression/test_level_in_cycle.py
+++ tests/regression/test_level_in_cycle.py
@@ -1,12 +1,14 @@
-import pytest
 from Constants import DEFAULT_NETWORK_CONFIG_MAP
 from api.block_api import BlockApi
+
 
 class DummyApiImpl(BlockApi):
     def __init__(self):
         super(DummyApiImpl, self).__init__(DEFAULT_NETWORK_CONFIG_MAP['MAINNET'])
+
     def get_current_cycle_and_level(self):
         return 0
+
 
 def test_levels_in_cycle():
 
@@ -14,11 +16,11 @@ def test_levels_in_cycle():
     # map is based on current mainnet values
 
     level_positions = [
-        [ 1589249, 0],       # Granada activation level
-        [ 1665240, 2263 ],
-	]
+        [1589249, 0],       # Granada activation level
+        [1665240, 2263],
+    ]
 
     block = DummyApiImpl()
 
-    for l in level_positions:
-        assert block.level_in_cycle(l[0]) == l[1]
+    for (level, pos) in level_positions:
+        assert block.level_in_cycle(level) == pos
