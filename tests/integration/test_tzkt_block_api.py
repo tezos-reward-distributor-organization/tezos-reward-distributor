b3299f998d26928f125e1c06acf127a91bfd4aae
Viktor Kreschenski <viktor.kreschenski@gmail.com>
2021-12-24 16:32:41 +0100

    Implemented rpc block api tests

--- tests/integration/test_tzkt_block_api.py
+++ tests/integration/test_tzkt_block_api.py
@@ -1,9 +1,9 @@
-from NetworkConfiguration import default_network_config_map
+from Constants import DEFAULT_NETWORK_CONFIG_MAP
 from tzkt.tzkt_block_api import TzKTBlockApiImpl
 
 
 def test_get_head():
-    tzkt_impl = TzKTBlockApiImpl(nw=default_network_config_map["MAINNET"])
+    tzkt_impl = TzKTBlockApiImpl(nw=DEFAULT_NETWORK_CONFIG_MAP["MAINNET"])
     (cycle, level) = tzkt_impl.get_current_cycle_and_level()
     assert cycle > 300
     assert level > 900000
