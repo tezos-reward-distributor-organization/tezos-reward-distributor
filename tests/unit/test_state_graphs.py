eac34d3785dbbf9fa914fc427a2225aaf714c409
jdsika <carlo.van-driesten@vdl.digital>
2022-03-28 15:27:53 +0200

    fix attempt place correct path for test

--- tests/unit/test_state_graphs.py
+++ tests/unit/test_state_graphs.py
@@ -2,22 +2,34 @@ import os
 
 
 def test_draw_process_life_cycle():
+    directory_path = os.path.dirname(os.path.abspath(__file__))
+    file_name = "launch_state_diagram.png"
+    file_path = os.path.join(directory_path, file_name)
     try:
         from util.process_life_cycle import ProcessLifeCycle
 
         process_life_cycle = ProcessLifeCycle(None)
-        process_life_cycle.get_fsm_builder().draw("launch_state_diagram.png")
-        assert os.path.isfile("launch_state_diagram.png") is True
+        process_life_cycle.get_fsm_builder().draw(file_path)
+        assert os.path.isfile(file_path) is True
     finally:
-        os.remove("launch_state_diagram.png")
+        try:
+            os.remove(file_path)
+        except OSError:
+            assert "Unable to remove file. Please investigate."
 
 
 def test_draw_config_life_cycle():
+    directory_path = os.path.dirname(os.path.abspath(__file__))
+    file_name = "config_cycle_state_diagram.png"
+    file_path = os.path.join(directory_path, file_name)
     try:
         from util.config_life_cycle import ConfigLifeCycle
 
         config_life_cycle = ConfigLifeCycle(None, None, None, None)
-        config_life_cycle.get_fsm_builder().draw("config_cycle_state_diagram.png")
-        assert os.path.isfile("config_cycle_state_diagram.png") is True
+        config_life_cycle.get_fsm_builder().draw(file_path)
+        assert os.path.isfile(file_path) is True
     finally:
-        os.remove("config_cycle_state_diagram.png")
+        try:
+            os.remove(file_path)
+        except OSError:
+            assert "Unable to remove file. Please investigate."
