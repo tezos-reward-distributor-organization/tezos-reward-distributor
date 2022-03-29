ce254bfe516b67ff873e0a65b7cd6a8df0400ee2
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-03-29 11:28:54 -0700

    fix api constants for tzstats

--- src/tzstats/tzstats_api_constants.py
+++ src/tzstats/tzstats_api_constants.py
@@ -3,10 +3,10 @@ idx_income_expected_income = 18
 idx_income_total_income = 19
 idx_income_total_bonds = 20
 
-idx_income_baking_income = 23
-idx_income_endorsing_income = 24
-idx_income_double_baking_income = 21
-idx_income_double_endorsing_income = 22
+idx_income_baking_income = 21
+idx_income_endorsing_income = 22
+idx_income_double_baking_income = 23
+idx_income_double_endorsing_income = 22 # missing, replaced by accusation income
 idx_income_seed_income = 24
 idx_income_fees_income = 25
 idx_income_missed_baking_income = 29  # Missing for now?
@@ -21,8 +21,8 @@ idx_income_lost_revelation_fees = 36  # missing?
 idx_income_lost_revelation_rewards = 37  # missing?
 
 # Rights
-idx_n_baking_rights = 8
-idx_n_endorsing_rights = 9
+idx_n_baking_rights = 9
+idx_n_endorsing_rights = 10
 
 # Cycle Snapshot
 idx_balance = 0
