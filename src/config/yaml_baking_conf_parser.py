b9a143154c74213990057580b40bdc3ab648ff96
Ryan <ryan.vermooten@vdl.digital>
2022-12-05 10:56:29 +0200

    place text back

--- src/config/yaml_baking_conf_parser.py
+++ src/config/yaml_baking_conf_parser.py
@@ -203,7 +203,7 @@ class BakingYamlConfParser(YamlConfParser):
 
         if pymnt_addr.startswith("KT"):
             raise ConfigurationException(
-                "KTs addresses cannot be used for payments. Only tz addresses are allowed"
+                "KT addresses cannot be used for payments. Only tz addresses are allowed"
             )
 
         if len(pymnt_addr) == PKH_LENGHT and pymnt_addr.startswith("tz"):
