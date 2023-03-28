b618852b5e7e9aafe97d27f52d83d7441ac532b8
jdsika <carlo.van-driesten@vdl.digital>
2023-03-28 18:13:56 +0200

    added correct NO_SPACE exit code in tests/utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -72,9 +72,10 @@ def store_reward_model(
             error_msg = "Exception during write operation invoked: {}. Not enough space on device.".format(
                 e
             )
+            exit_program(ExitCode.NO_SPACE, error_msg)
         else:
             error_msg = "Exception during write operation invoked: {}".format(e)
-        exit_program(ExitCode.NO_SPACE, error_msg)
+            exit_program(ExitCode.GENERAL_ERROR, error_msg)
 
 
 class Args:
