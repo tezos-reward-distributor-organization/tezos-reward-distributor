fb1351e6348eac3e51acdcf0d17b1958d04dab2a
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-07-31 07:55:06 -0700

    add potential Oxford date

--- src/Constants.py
+++ src/Constants.py
@@ -20,8 +20,8 @@ TEMP_TEST_DATA_DIR = "__TEMP_DATA__"
 REQUIREMENTS_FILE_PATH = "requirements.txt"
 
 # potentially the next upgrade
-NEW_PROTOCOL_DATE = date(2023, 7, 31)
-NEW_PROTOCOL_NAME = "Nairobi"
+NEW_PROTOCOL_DATE = date(2023, 12, 31)
+NEW_PROTOCOL_NAME = "Oxford"
 
 LOCAL_HOST = "127.0.0.1"
 EXIT_PAYMENT_TYPE = "exit"
