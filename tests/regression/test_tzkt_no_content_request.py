5258a33596787bc0e4c743f0d32b72c0097f02a1
Viktor <viktor.kreschenski@gmail.com>
2024-01-05 00:55:38 +0100

    Final Fix

--- tests/regression/test_tzkt_no_content_request.py
+++ tests/regression/test_tzkt_no_content_request.py
@@ -1,4 +1,5 @@
 import pytest
+import vcr
 from http import HTTPStatus
 from unittest.mock import patch, MagicMock
 from src.tzkt.tzkt_api import TzKTApi, TzKTApiError
@@ -42,6 +43,10 @@ def test_request_dns_lookup_error():
         _ = tzkt._request(request_path, offset=0, limit=10000)
 
 
+@vcr.use_cassette(
+    "tests/regression/cassettes/test_request_content_response.yaml",
+    filter_headers=["X-API-Key", "authorization"],
+)
 def test_request_content_response():
     """Test the handling of API calls which respond with a content (200)."""
     baker_address = "tz1NortRftucvAkD1J58L32EhSVrQEWJCEnB"
