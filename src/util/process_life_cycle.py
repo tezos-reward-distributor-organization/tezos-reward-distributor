688ba2a7b71175943ffaba175e58da33322b34ec
Nicolas Ochem <nicolas.ochem@gmail.com>
2024-02-07 09:54:40 -0800

    lint

--- src/util/process_life_cycle.py
+++ src/util/process_life_cycle.py
@@ -5,7 +5,7 @@ import signal
 import platform
 from _signal import SIGABRT, SIGILL, SIGSEGV, SIGTERM
 if platform.system() != 'Windows':
-   from _signal import SIGUSR1, SIGUSR2
+    from _signal import SIGUSR1, SIGUSR2
 from enum import Enum, auto
 from time import sleep
 
