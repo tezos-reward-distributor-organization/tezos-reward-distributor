0a666c3ba9359db62ca96bbfa1c85d4a969691ad
Nicolas Ochem <nicolas.ochem@gmail.com>
2024-06-27 22:20:15 -0700

    set new protocol date, do not confirm when passed, fix #701 (#705)

--- src/Constants.py
+++ src/Constants.py
@@ -20,8 +20,8 @@ TEMP_TEST_DATA_DIR = "__TEMP_DATA__"
 REQUIREMENTS_FILE_PATH = "requirements.txt"
 
 # potentially the next upgrade
-NEW_PROTOCOL_DATE = date(2024, 6, 30)
-NEW_PROTOCOL_NAME = "P_______"
+NEW_PROTOCOL_DATE = date(2024, 12, 31)
+NEW_PROTOCOL_NAME = "Q"
 
 LOCAL_HOST = "127.0.0.1"
 EXIT_PAYMENT_TYPE = "exit"
